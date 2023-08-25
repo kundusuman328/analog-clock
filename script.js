013d64b00d38af41859a931f1ddf8d8643fad2dd
Suman <kundusuman328@gmail.com>
2023-08-25 13:42:33 +0530

    sound added

--- script.js
+++ script.js
@@ -12,6 +12,14 @@ function showClock() {
   h.style.transform = `rotate(${hour * 30 + (min / 60) * 30}deg)`;
   m.style.transform = `rotate(${min * 6 + (sec / 60) * 6}deg)`;
   s.style.transform = `rotate(${sec * 6}deg)`;
+
+  if (sec % 2 == 0) {
+    let sound = new Audio("tik.mp3");
+    sound.play();
+  } else {
+    let sound = new Audio("tok.mp3");
+    sound.play();
+  }
 }
 
 setInterval(showClock, 1000);
